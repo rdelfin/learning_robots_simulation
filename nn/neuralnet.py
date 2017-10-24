25cf758d0bacb726eacf15b889b024d88216206c
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-10-24 12:28:21 -0500

    Added basic neural network skeleton

---
+++ nn/neuralnet.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+class NeuralNet:
+    def __init__(self, layers, sizes):
+        pass
+
+    def add_sample(self, sample_x, sample_y):
+        pass
+
+    def train(self):
+        pass
+
+    def get_weights(self):
+        pass
+
+    def predict(self, input_x):
+        pass
+
+    def cost(self):
+        pass
+
+    def accuracy(self, test_set):
+        pass
