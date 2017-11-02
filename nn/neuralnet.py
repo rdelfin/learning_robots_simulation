1722b55a495e6b0a80026cc186d6dea7c8147576
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-01 22:53:05 -0500

    Added functions for getting matrices

--- nn/neuralnet.py
+++ nn/neuralnet.py
@@ -1,35 +1,45 @@
-import tensorflow as tf
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+import numpy as np
 
 class NeuralNet:
-    def __init__(self, layers, sizes):
-        sess = tf.InteractiveSession()
+    """
+    Represents a feed forward, fully connected neural network that can be trained using
+    stochastic gradient descent. This one makes use of the sigmoid activation function
+    for all but the last neuron, which uses a linear activation.
+    """
+    def __init__(self, sizes):
+        self.sizes = list(sizes)
+        self.layers = len(sizes)
+        self.reset_weights()
 
-        self.x = tf.placeholder(tf.float32, shape=[None, sizes[0]])
-        self.y_ = tf.placeholder(tf.float32, shape=[None, sizes[-1]])
+    def reset_weights(self):
+        """
+        Resets all the weights in the neural network to a normal distribution around
+        0 with standard deviation of 0.1.
+        """
+        self.weights = []
+        for i in range(self.layers - 1):
+            self.weights = np.append(self.weights,
+                                     np.random.normal(scale=0.1, size=tuple(self.sizes[i:i+2])))
 
-        self.weight_list = []
-        self.bias_list = []
-        self.fc = []
+    def get_layer_weights(self, layer):
+        """
+        Gets the matrix of weights between a given layer and the next layer from the
+        array self.weights. The dimensions are set up so that you can calculate the
+        next layer's activation as:
+        ```
+        activation(get_layer_weights(layer) * last_layer_activation_vector)
+        ```
+        Where last_layer_activation_vector is a column (aka vertical) vector.
+        """
+        dims = (self.sizes[layer+1], self.sizes[layer])
+        size = dims[0] * dims[1]
+        next_pairs = ((self.sizes[i], self.sizes[i+1]) for i in self.layers - 1)
+        offset = sum(pair[0] * pair[1] for pair in next_pairs)
 
-        for i in range(1, layers):
-            self.weight_list += weight_variable([sizes[i], sizes[i+1]])
-            self.bias_list += bias_variable([sizes[i+1]])
+        weight_subset = self.weights[offset:offset+size]
+        weight_subset.resize(dims)
 
-            prev_vals = self.x if i == 0 else self.fc[i-1]
-            if i != layers - 1:
-                self.fc += tf.nn.sigmoid(tf.matmul(prev_vals, self.weight_list[-1]) + self.bias_list[-1])
-            else:
-                self.fc += tf.matmul(prev_vals, self.weight_list[-1]) + self.bias_list[-1]
-
-        sess.run(tf.global_variables_initializer())
+        return np.matrix(weight_subset)
 
     def add_sample(self, sample_x, sample_y):
         pass
