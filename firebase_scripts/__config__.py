5a3aef78f2a7f4fcb2e37dce436fab1160d59114
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-02 13:15:40 -0500

    Added travel_data functions to fetch and manipulate travel data

---
+++ firebase_scripts/__config__.py
@@ -0,0 +1,6 @@
+firebase_data = {
+    "apiKey": None,
+    "authDomain": "robotimages-dacc9.appspot.com",
+    "databaseURL": "https://robotimages-dacc9.firebaseio.com",
+    "storageBucket": "robotimages-dacc9.appspot.com"
+}
