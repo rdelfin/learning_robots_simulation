27e8cb8a03588667489ce6e32f9ad6a33f2e1547
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-10 19:27:55 -0600

    Partial MDP states commit

---
+++ mdp/states.py
@@ -0,0 +1,10 @@
+class FullState:
+    def __init__(self, *, location, time_of_day, day_of_week, person_present_map, request_history):
+        self.location = location
+        self.time_of_day = time_of_day
+        self.day_of_week = day_of_week
+        self.person_present_map = person_present_map
+        self.request_history = request_history
+
+    def time_only_state(self):
+        return (self.time_of_day, self.day_of_week)
