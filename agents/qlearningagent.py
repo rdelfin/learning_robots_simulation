b2717804ce3570b39c831957b52a80da3279ab95
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-27 20:54:58 -0600

    Added empty qlearning agent

---
+++ agents/qlearningagent.py
@@ -0,0 +1,11 @@
+from agents.agent import Agent
+
+class QLearningAgent(Agent):
+    def __init__(self):
+        pass
+    
+    def next_action(self, state):
+        pass
+
+    def action_update(self, reward, new_state):
+        pass
