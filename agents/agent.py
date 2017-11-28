773665030d3ebd126a51486712deb068fde0ea96
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-27 20:54:43 -0600

    Added comment to agent.py

--- agents/agent.py
+++ agents/agent.py
@@ -7,4 +7,9 @@ class Agent:
         raise NotImplementedError
     
     def action_update(self, reward, new_state):
+        """
+        Method will be called to update the agent after the
+        last action taken by next_action to give new state
+        and reward
+        """
         raise NotImplementedError
