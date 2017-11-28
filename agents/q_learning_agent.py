f5e7540ae1691ff360cdd4dd65d92fb449904ec3
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-28 12:27:30 -0600

    Replaced q_learning_agent to use Function estimator

--- agents/q_learning_agent.py
+++ agents/q_learning_agent.py
@@ -1,6 +1,8 @@
 from agents.agent import Agent
 from nn.neuralnet import NeuralNet
 from firebase_scripts import travel_data
+from agents.function_estimator import FunctionEstimator, NeuralNetEstimator
+from mdp.states import Action, FullState
 
 from functools import reduce
 import random
@@ -8,7 +10,7 @@ import random
 import numpy as np
 
 class QLearningAgent(Agent):
-    def __init__(self, eps):
+    def __init__(self, eps, estimator: FunctionEstimator):
         # FIX: Stop depending on states class to define sizes
         # 8 input neurons:
         #   1 for time
@@ -18,29 +20,15 @@ class QLearningAgent(Agent):
         self.num_actions = len(self.travel_locations) - 1
         self.neural_net = NeuralNet([8 + self.num_actions, 20, 10, 1], 0.9)
         self.eps = eps
+        self.estimator = estimator
 
-    def next_action(self, state):
-        max_location = max(self.travel_locations, key=lambda loc: self.neural_net.predict(self.to_nn_input(loc, state))[0, 0])
+    def next_action(self, state: FullState):
+        max_location = max(self.travel_locations, key=lambda loc: self.estimator.get_qval(state, Action(loc, True)))
         rand_location = random.choice(self.travel_locations)
 
         self.action_taken = rand_location if random.random() < self.eps else max_location   # Epsilon-greedy behaviour
         return self.action_taken
-    
-    def to_nn_input(self, location, state):
-        nn_input = np.array([0.0 for x in range(8 + self.num_actions)], np.float64)
-        func_state = state.time_only_state()
-        travel_id_pairs = zip(self.travel_locations, range(len(self.travel_locations)))
-        location_idx = reduce(lambda x, y: y[1] if y[0] == location else x, travel_id_pairs, -1)
-        if location_idx < 0: # Should never happen
-            raise IndexError
 
-        nn_input[0] = func_state[0]            # Add time of day to state.
-        nn_input[1 + func_state[1]] = 1.0      # set to 1 the neuron corresponding to the current day (skipping the first element).
-        nn_input[8 + location_idx] = 1.0       # Set 1 to the neuron corresponding to the action taken.
-
-        return np.mat(nn_input)
-
-
-    def action_update(self, reward, new_state):
+    def action_update(self, reward, new_state: FullState):
         # TODO: Implement update (page 198 of Sutton http://incompleteideas.net/sutton/book/bookdraft2017nov5.pdf)
         pass
