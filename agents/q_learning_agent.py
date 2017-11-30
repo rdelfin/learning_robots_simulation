fd33543d621785b9b9d64724d7c4f20aefefda4d
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-11-30 01:26:06 -0600

    Implemented action update as a semi-gradient SARSA update

--- agents/q_learning_agent.py
+++ agents/q_learning_agent.py
@@ -1,8 +1,7 @@
 from agents.agent import Agent
-from nn.neuralnet import NeuralNet
 from firebase_scripts import travel_data
-from agents.function_estimator import FunctionEstimator, NeuralNetEstimator
-from mdp.states import Action, FullState
+from agents.function_estimator import FunctionEstimator
+from mdp.states import Action, FullState, is_terminal_state
 
 from functools import reduce
 import random
@@ -10,7 +9,7 @@ import random
 import numpy as np
 
 class QLearningAgent(Agent):
-    def __init__(self, eps, estimator: FunctionEstimator):
+    def __init__(self, eps, alpha, gamma, estimator: FunctionEstimator):
         # FIX: Stop depending on states class to define sizes
         # 8 input neurons:
         #   1 for time
@@ -18,17 +17,39 @@ class QLearningAgent(Agent):
         #   num_actions for each action (move to location, excluding the current location)
         self.travel_locations = travel_data.get_location_ids()
         self.num_actions = len(self.travel_locations) - 1
-        self.neural_net = NeuralNet([8 + self.num_actions, 20, 10, 1], 0.9)
         self.eps = eps
+        self.alpha = alpha
+        self.gamma = gamma
         self.estimator = estimator
-
-    def next_action(self, state: FullState):
+        self.future_state_actions = []
+        self.past_state_actions = []
+    
+    def generate_next_action(self, state: FullState):
         max_location = max(self.travel_locations, key=lambda loc: self.estimator.get_qval(state, Action(loc, True)))
         rand_location = random.choice(self.travel_locations)
 
-        self.action_taken = rand_location if random.random() < self.eps else max_location   # Epsilon-greedy behaviour
-        return self.action_taken
+        action_taken = rand_location if random.random() < self.eps else max_location   # Epsilon-greedy behaviour
+        
+        self.future_state_actions += (state, action_taken)
+
+    def next_action(self, state: FullState):
+        if(not self.future_state_actions):
+            self.generate_next_action(state)
+        
+        new_action = self.future_state_actions[0][1]
+        self.past_state_actions += self.future_state_actions[0]
+        del self.future_state_actions[0]
+        return new_action
 
     def action_update(self, reward, new_state: FullState):
-        # TODO: Implement update (page 198 of Sutton http://incompleteideas.net/sutton/book/bookdraft2017nov5.pdf)
-        pass
+        # Implemented semi-gradient SARSA update
+        # (page 198 of Sutton http://incompleteideas.net/sutton/book/bookdraft2017nov5.pdf)
+        if is_terminal_state(new_state):
+            qval_diff = reward - self.estimator.get_qval(*self.past_state_actions[-1])
+        else:
+            self.generate_next_action(new_state)
+            next_sa_pair = self.future_state_actions[0]
+            qval_diff = reward + self.gamma * self.estimator.get_qval(*next_sa_pair)
+
+        self.estimator.set_weights(self.estimator.get_weights() + self.estimator.get_grad() * self.alpha * qval_diff)
+        
